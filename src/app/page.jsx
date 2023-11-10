b663318a5c7b17a1d2e631533e721a4ac3e68316
Johanna Magdalena <69107842+johannamag@users.noreply.github.com>
2023-11-10 14:53:56 +0100

    hero

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,10 @@
 import Events from "@/components/events/Events";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
     <main>
+      <Hero />
       <Events />
     </main>
   );
