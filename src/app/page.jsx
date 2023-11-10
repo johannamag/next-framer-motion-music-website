74d19efb3832da183ec2e6b7d855bde0de80b315
Johanna Magdalena <69107842+johannamag@users.noreply.github.com>
2023-11-10 12:54:11 +0100

    Events

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,9 @@
-import Image from 'next/image'
+import Events from "@/components/events/Events";
 
 export default function Home() {
   return (
-    <main>Home</main>
-  )
+    <main>
+      <Events />
+    </main>
+  );
 }
--- src/components/events/Events.jsx
+++ src/components/events/Events.jsx
@@ -1,7 +1,18 @@
-const Events = () => {
-  return (
-    <div>Events</div>
-  )
-}
+import EventBox from "./EventBox";
 
-export default Events
+const getEvents = async () => {
+  const res = await fetch("http://localhost:4000/events");
+  return res.json()
+};
+
+const Events = async () => {
+  const events = await getEvents()
+
+  return <section className="section" id="tours">
+    <div className="container mx-auto">
+      <EventBox events={events} />
+    </div>
+  </section>;
+};
+
+export default Events;
