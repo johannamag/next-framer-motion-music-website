a4988785925d3d471a213cc7a2dfee3ec7555712
Johanna Magdalena <69107842+johannamag@users.noreply.github.com>
2023-11-12 09:34:57 +0100

    player

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,12 @@
 import Events from "@/components/events/Events";
 import Hero from "@/components/Hero";
+import Player from "@/components/Player";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <Player />
       <Events />
     </main>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className="fixed z-50 w-full">Header</header>
   )
 }
 
